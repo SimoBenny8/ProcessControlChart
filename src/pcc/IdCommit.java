57bad54f684f806871bf1d0e31e38f0b177c2982
SimoBenny8 <79932425+SimoBenny8@users.noreply.github.com>
2021-03-22 18:09:59 +0100

    bug fixed

--- src/pcc/IdCommit.java
+++ src/pcc/IdCommit.java
@@ -13,6 +13,10 @@ public class IdCommit {
 	private static final Logger logger = Logger.getLogger(IdCommit.class.getName());
 	private static final String ERRORSTR = "Exception found";
 	
+	private IdCommit() {
+	    throw new IllegalStateException("Utility class");
+	}
+	
 	public static void commitString(String wordToSearch, String project,String fileName, int i) throws IOException {
 		
 		String s;
@@ -34,6 +38,7 @@ public class IdCommit {
                 
         	 }catch (Exception e) {
             	logger.log(Level.WARNING,ERRORSTR);
+            	p.destroy();
         	 }
          }else {
         	 try(FileWriter result = new FileWriter(outname)){
@@ -47,6 +52,7 @@ public class IdCommit {
                  
              }catch (Exception e) {
              	logger.log(Level.WARNING,ERRORSTR);
+             	p.destroy();
              }
          }
          
@@ -56,32 +62,7 @@ public class IdCommit {
 		
 	
 
-	public static void main(String[] args) {
-		String s;
-        Process p;
-        String keyword = "commit";
-        String project = "C:\\Users\\Simone Benedetti\\Documents\\Programmazione JAVA\\Bahir";
-        String wordToSearch = "BAHIR-49";
-        
-        try {
-            p = Runtime.getRuntime().exec("cmd /c cd "+project+"&& git log --grep="+wordToSearch+" --date=iso-strict --name-status --stat HEAD --abbrev-commit");
-            BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
-            while ((s = br.readLine()) != null) {
-            	if (s.toLowerCase().indexOf(keyword.toLowerCase()) != -1 ) {
-            		String[] words = s.split(" ");
-            		String id = words[1];
-            		logger.log(Level.INFO, "Commit id: {0}", id);
-            	}
-            }	
-            p.waitFor();
-            p.destroy();
-            
-        }catch (Exception e) {
-        	logger.log(Level.WARNING,ERRORSTR);
-        }
-        
-    }
-    
+	
 
 }
 

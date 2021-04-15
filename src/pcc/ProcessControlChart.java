0ef2f863a0998bc5631181ebc2fa53c7ebe122a5
SimoBenny8 <79932425+SimoBenny8@users.noreply.github.com>
2021-04-15 12:12:26 +0200

    added automatic clone rep

--- src/pcc/ProcessControlChart.java
+++ src/pcc/ProcessControlChart.java
@@ -2,6 +2,8 @@ package pcc;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.json.JSONException;
 
@@ -9,14 +11,30 @@ public class ProcessControlChart {
 	
 	
 	private static ArrayList<String> id;
+	private static final Logger logger = Logger.getLogger(ProcessControlChart.class.getName());
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException, InterruptedException {
+		
 		
 		String progName = "BAHIR";
-		String project1 = "C:\\Users\\Simone Benedetti\\Documents\\Programmazione JAVA\\Bahir";
-		String project2 = "C:\\Users\\Simone Benedetti\\Documents\\Programmazione JAVA\\Bahir-website";
-		String project3 = "C:\\Users\\Simone Benedetti\\Documents\\Programmazione JAVA\\Flink";
+		String pathBahir = "bahir";
+		String pathFlink = "bahir-flink";
+		String pathName = "C:/Users/"+ OsUtils.getUserName()+"/Documents/";
+		Process p = null;
+
 		
+		if(OsUtils.isWindows()) {
+			//GIT required
+			p = Runtime.getRuntime().exec("cmd /c cd "+ pathName +"&& git clone https://github.com/apache/bahir-flink"+"&& git clone https://github.com/apache/bahir");
+		}
+		else {
+			logger.log(Level.WARNING,"Windows Required");
+		}
+		if(p != null) {
+			
+			p.waitFor();
+			p.destroy();
+		}
 		try {
 			id = (ArrayList<String>) RetrieveTicketsID.getIdCommit(progName);
 		} catch (JSONException|IOException e) {
@@ -27,9 +45,8 @@ public class ProcessControlChart {
 		
 		for(int i=0; i<id.size(); i++) {
 			try {
-				IdCommit.commitString(id.get(i), project1, "bahir" , i);
-				IdCommit.commitString(id.get(i), project2,"bahir-website", i);
-				IdCommit.commitString(id.get(i), project3, "flink",i);
+				IdCommit.commitString(id.get(i), pathName + pathBahir, pathBahir , i);
+				IdCommit.commitString(id.get(i), pathName + pathFlink, pathFlink ,i);
 			} catch (IOException e) {
 				
 				e.printStackTrace();
